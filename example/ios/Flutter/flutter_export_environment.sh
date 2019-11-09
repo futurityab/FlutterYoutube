15ff78b40063e10bf78972ea107ab86bc08a1ff4
Fredrik Hansen <fredrik.hansen@futurity.se>
2019-11-09 19:33:53 +0100

    auto commited

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,8 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/pki/Documents/flutterSDK"
 export "FLUTTER_APPLICATION_PATH=/Users/pki/Documents/GitHub/FlutterYoutube/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/pki/Documents/GitHub/FlutterYoutube/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/pki/Documents/flutterSDK/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"

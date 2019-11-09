31d6d3965d2fbf661cb492d6eb1ed4ae61aed797
Fredrik Hansen <fredrik.hansen@futurity.se>
2019-11-09 20:31:25 +0100

    removed icon package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_youtube/flutter_youtube.dart';
-import 'package:flutter_launcher_icons/android.dart';
-import 'package:flutter_launcher_icons/constants.dart';
-import 'package:flutter_launcher_icons/custom_exceptions.dart';
-import 'package:flutter_launcher_icons/ios.dart';
-import 'package:flutter_launcher_icons/main.dart';
-import 'package:flutter_launcher_icons/xml_templates.dart';
+
 
 void main() => runApp(new MyApp());
 

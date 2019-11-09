fa9986c9cd558f2848af0468e000878b9b637fb7
Fredrik Hansen <fredrik.hansen@futurity.se>
2019-11-09 18:23:34 +0100

    pub get

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ponna\Desktop\Projects\plugins\FlutterYoutube\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/pki/Documents/flutterSDK"
+export "FLUTTER_APPLICATION_PATH=/Users/pki/Documents/GitHub/FlutterYoutube/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=E:\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/pki/Documents/flutterSDK/bin/cache/artifacts/engine/ios"
